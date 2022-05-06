d1ca8498e402af9336a1acf50997173fce63e6a6
hallvaaw <hallvard_wahler@hotmail.com>
2022-05-06 23:26:34 +0200

    added colour number buttons

--- script.js
+++ script.js
@@ -3,14 +3,13 @@
 let count = 0;
 
 let paletteCount = document.createElement("p");
-paletteCount.id = "palette-count";
 paletteCount.innerText = count;
 
 const addCount = () => {
     count += 1;
-    console.log(count);
     paletteCount.innerText = count;
 }
+
 
 // buttons
 let likeBtn = document.createElement("button");
@@ -20,6 +19,34 @@ let dislikeBtn = document.createElement("button");
 dislikeBtn.innerText = "Dislike";
 let finishBtn = document.createElement("button");
 finishBtn.innerText = "Done";
+
+// number of colours
+let colInt = 2; // default is two colours
+let selectedColInfo = document.createElement("p");
+let colInfo = document.createElement("h2");
+selectedColInfo.innerText = `Showing palette with ${colInt} colours`;
+
+let twoBtn = document.createElement("button");
+twoBtn.addEventListener('click', function(){updateColInt(2)});
+twoBtn.innerText = "Two colours"
+
+let threeBtn = document.createElement("button");
+threeBtn.addEventListener('click', function(){updateColInt(3)});
+threeBtn.innerText = "Three colours"
+
+let fourBtn = document.createElement("button");
+fourBtn.addEventListener('click', function(){updateColInt(4)});
+fourBtn.innerText = "Four colours"
+
+let fiveBtn = document.createElement("button");
+fiveBtn.addEventListener('click', function(){updateColInt(5)});
+fiveBtn.innerText = "Five colours"
+
+
+const updateColInt = (newInt) => {
+    colInt = newInt;
+    selectedColInfo.innerText = `Showing palette with ${colInt} colours`;
+};
 
 
 // display palettes
@@ -46,7 +73,15 @@ function newTable(color1, color2, color3, color4, color5) {
     }
 }
 
-let paletteTable2 = new newTable("red", "blue", "white", "white", "white");
+let paletteTable = new newTable("red", "blue", "white", "white", "white");
+
+
+
+document.body.appendChild(twoBtn);
+document.body.appendChild(threeBtn);
+document.body.appendChild(fourBtn);
+document.body.appendChild(fiveBtn);
+document.body.appendChild(selectedColInfo);
 
 
 document.body.appendChild(dislikeBtn);
